e1d62315ddd7a9e3b16f778f4ec5817749889d66
Henrique Claranhan <henrique.claranhan@outlook.com>
2024-06-27 19:35:00 -0400

    Redireciona url root para login

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 from core import settings
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path("", include("user.urls")),
     path("", include("book.urls")),
     path("", include("transactions.urls")),
+    path("", RedirectView.as_view(url="/login")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)
